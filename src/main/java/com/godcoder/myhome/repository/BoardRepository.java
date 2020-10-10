c6d827483864f8c6dd7574d2ab41b81e5768486c
LKHcoding <tha920@naver.com>
2020-10-10 21:03:22 +0900

    boardrepository 오류 해결 및 게시판 조회, 글작성, 글 수정까지 구현 완료, rest api 기능 구현 해야함.

--- src/main/java/com/godcoder/myhome/repository/BoardRepository.java
+++ src/main/java/com/godcoder/myhome/repository/BoardRepository.java
@@ -1,10 +1,7 @@
 package com.godcoder.myhome.repository;
-
+import com.godcoder.myhome.model.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.godcoder.myhome.model.Board;
-
 public interface BoardRepository extends JpaRepository<Board, Long> {
-
 
 }
